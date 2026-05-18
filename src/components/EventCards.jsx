618643912d4447d63e22a2b3f2a84101e6a70cfb
ademola <ademola.idrisakinola1110@gmail.com>
2026-05-18 14:26:24 +0100

    adjustment

--- src/components/EventCards.jsx
+++ src/components/EventCards.jsx
@@ -34,7 +34,7 @@ function Events() {
       </div>
 
       {/* Events Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {filteredEvents.length > 0 ? (
           filteredEvents.map((event) => (
             <div
